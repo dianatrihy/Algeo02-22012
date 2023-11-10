52f37d78066e241ebdb53ecb776af9b5d7b7abc2
dianatrihy <dianatrihy@gmail.com>
2023-11-10 12:03:00 +0700

    folor function

--- api/venv/color.py
+++ api/venv/color.py
@@ -3,192 +3,202 @@ import cv2
 import time
 import functions as fn
 
-start_time = time.time()
+# start_time = time.time()
 
 # path = "C:\Users\HP\tubes-algeo2\dataset\0.jpg"
-image = cv2.imread("C:/Users/HP/tubes-algeo2/dataset/0.jpg")
+# image = cv2.imread("C:/Users/HP/tubes-algeo2/dataset/0.jpg")
 
-R = [[0 for i in range(len(image))] for j in range (len(image))]
-G = [[0 for i in range(len(image))] for j in range (len(image))]
-B = [[0 for i in range(len(image))] for j in range (len(image))]
+def color(path):
+    image = cv2.imread(path)
+    # image = np.resize(image, (256,256))
+    R = [[0 for i in range(len(image[0]))] for j in range (len(image))]
+    G = [[0 for i in range(len(image[0]))] for j in range (len(image))]
+    B = [[0 for i in range(len(image[0]))] for j in range (len(image))]
 
-# denorm
-for i in range (len(image)):
-    for j in range (len(image)):
-        R[i][j] = image[i][j][2] / 255
-        G[i][j] = image[i][j][1] / 255
-        B[i][j] = image[i][j][0] / 255
+    # denorm
+    for i in range (len(image)):
+        for j in range (len(image[0])):
+            R[i][j] = image[i][j][2] / 255
+            G[i][j] = image[i][j][1] / 255
+            B[i][j] = image[i][j][0] / 255
 
-# resize
-R = np.resize(R,(256,256))
-G = np.resize(G,(256,256))
-B = np.resize(B,(256,256))
+    # resize
+    re = 60
+    R = np.resize(R,(re,re))
+    G = np.resize(G,(re,re))
+    B = np.resize(B,(re,re))
 
-# find cmax, cmin, delta
-Cmax = [[0 for i in range(256)] for j in range (256)]
-Cmin = [[0 for i in range(256)] for j in range (256)]
-delta = [[0 for i in range(256)] for j in range (256)]
+    # find cmax, cmin, delta
+    Cmax = [[0 for i in range(re)] for j in range (re)]
+    Cmin = [[0 for i in range(re)] for j in range (re)]
+    delta = [[0 for i in range(re)] for j in range (re)]
 
-for i in range (256):
-    for j in range (256):
-        Cmax[i][j] = max(R[i][j],G[i][j],B[i][j])
-        Cmin[i][j] = min(R[i][j],G[i][j],B[i][j])
-        delta[i][j] = Cmax[i][j] - Cmin[i][j]
+    for i in range (re):
+        for j in range (re):
+            Cmax[i][j] = max(R[i][j],G[i][j],B[i][j])
+            Cmin[i][j] = min(R[i][j],G[i][j],B[i][j])
+            delta[i][j] = Cmax[i][j] - Cmin[i][j]
 
-# convert to hsv
-H = [[0 for i in range(256)] for j in range (256)]
-S = [[0 for i in range(256)] for j in range (256)]
-V = [[0 for i in range(256)] for j in range (256)]
+    # convert to hsv
+    H = [[0 for i in range(re)] for j in range (re)]
+    S = [[0 for i in range(re)] for j in range (re)]
+    V = [[0 for i in range(re)] for j in range (re)]
 
-for i in range (256):
-    for j in range (256):
-        if (delta[i][j] == 0):
-            H[i][j] = 0
-        elif (Cmax[i][j] == R[i][j]):
-            H[i][j] = 60 * (((G[i][j]-B[i][j])/delta[i][j]) % 6)
-        elif (Cmax[i][j] == G[i][j]):
-            H[i][j] = 60 * (((B[i][j]-R[i][j])/delta[i][j]) + 2)
-        elif (Cmax[i][j] == B[i][j]):
-            H[i][j] = 60 * (((R[i][j]-G[i][j])/delta[i][j]) + 4)
-        
-        if (Cmax[i][j] == 0):
-            S[i][j] = 0
-        else:
-            S[i][j] = delta[i][j]/Cmax[i][j]
-        
-        V[i][j] = Cmax[i][j]
-
-# slicing into 3x3
-H = np.array(H)
-S = np.array(S)
-V = np.array(V)
-
-H1 = H[0:85, 0:85]
-S1 = S[0:85, 0:85]
-V1 = V[0:85, 0:85]
-
-H2 = H[0:85, 85:170]
-S2 = S[0:85, 85:170]
-V2 = V[0:85, 85:170]
-
-H3 = H[0:85, 170:256]
-S3 = S[0:85, 170:256]
-V3 = V[0:85, 170:256]
-
-H4 = H[85:170, 0:85]
-S4 = S[85:170, 0:85]
-V4 = V[85:170, 0:85]
-
-H5 = H[85:170, 85:170]
-S5 = S[85:170, 85:170]
-V5 = V[85:170, 85:170]
-
-H6 = H[85:170, 170:256]
-S6 = S[85:170, 170:256]
-V6 = V[85:170, 170:256]
-
-H7 = H[170:256, 0:85]
-S7 = S[170:256, 0:85]
-V7 = V[170:256, 0:85]
-
-H8 = H[170:256, 85:170]
-S8 = S[170:256, 85:170]
-V8 = V[170:256, 85:170]
-
-H9 = H[170:256, 170:256]
-S9 = S[170:256, 170:256]
-V9 = V[170:256, 170:256]
-
-def formula_H(arr, H):
-    for i in range (len(H)):
-        for j in range (len(H[0])):
-            if (H[i][j] >= 316 and H[i][j] <= 360):
-                arr[0] += 1
-            elif (H[i][j] >= 1 and H[i][j] < 26):
-                arr[1] += 1
-            elif (H[i][j] >= 26 and H[i][j] < 41):
-                arr[2] += 1
-            elif (H[i][j] >= 41 and H[i][j] < 121):
-                arr[3] += 1
-            elif (H[i][j] >= 121 and H[i][j] < 191):
-                arr[4] += 1
-            elif (H[i][j] >= 191 and H[i][j] < 271):
-                arr[5] += 1
-            elif (H[i][j] >= 271 and H[i][j] < 295):
-                arr[6] += 1
-            elif (H[i][j] >= 295 and H[i][j] < 316):
-                arr[7] += 1
+    for i in range (re):
+        for j in range (re):
+            if (delta[i][j] == 0):
+                H[i][j] = 0
+            elif (Cmax[i][j] == R[i][j]):
+                H[i][j] = 60 * (((G[i][j]-B[i][j])/delta[i][j]) % 6)
+            elif (Cmax[i][j] == G[i][j]):
+                H[i][j] = 60 * (((B[i][j]-R[i][j])/delta[i][j]) + 2)
+            elif (Cmax[i][j] == B[i][j]):
+                H[i][j] = 60 * (((R[i][j]-G[i][j])/delta[i][j]) + 4)
             
-def formula_S(arr, S):
-    for i in range (len(S)):
-        for j in range (len(S[0])):
-            if (S[i][j] >= 0 and S[i][j] < 0.2):
-                arr[8] += 1
-            elif (S[i][j] >= 0.2 and S[i][j] < 0.7):
-                arr[9] += 1
-            elif (S[i][j] >= 0.7 and S[i][j] <= 1):
-                arr[10] += 1   
-    
-def formula_V(arr, V):
-    for i in range (len(V)):
-        for j in range (len(V[0])):
-            if (V[i][j] >= 0 and V[i][j] < 0.2):
-                arr[11] += 1
-            elif (V[i][j] >= 0.2 and V[i][j] < 0.7):
-                arr[12] += 1
-            elif (V[i][j] >= 0.7 and V[i][j] <= 1):
-                arr[13] += 1    
+            if (Cmax[i][j] == 0):
+                S[i][j] = 0
+            else:
+                S[i][j] = delta[i][j]/Cmax[i][j]
+            
+            V[i][j] = Cmax[i][j]
 
-array1 = [0 for i in range(14)]
-formula_H(array1, H1)
-formula_S(array1, S1)
-formula_V(array1, V1)
+    # slicing into 3x3
+    H = np.array(H)
+    S = np.array(S)
+    V = np.array(V)
 
-array2 = [0 for i in range(14)]
-formula_H(array2, H2)
-formula_S(array2, S2)
-formula_V(array2, V2)
+    re1 = re//3
+    re2 = re//3 *2
+    re3 = re
 
-array3 = [0 for i in range(14)]
-formula_H(array3, H3)
-formula_S(array3, S3)
-formula_V(array3, V3)
+    H1 = H[0:re1, 0:re1]
+    S1 = S[0:re1, 0:re1]
+    V1 = V[0:re1, 0:re1]
 
-array4 = [0 for i in range(14)]
-formula_H(array4, H4)
-formula_S(array4, S4)
-formula_V(array4, V4)
+    H2 = H[0:re1, re1:re2]
+    S2 = S[0:re1, re1:re2]
+    V2 = V[0:re1, re1:re2]
 
-array5 = [0 for i in range(14)]
-formula_H(array5, H5)
-formula_S(array5, S5)
-formula_V(array5, V5)
+    H3 = H[0:re1, re2:re3]
+    S3 = S[0:re1, re2:re3]
+    V3 = V[0:re1, re2:re3]
 
-array6 = [0 for i in range(14)]
-formula_H(array6, H6)
-formula_S(array6, S6)
-formula_V(array6, V6)
+    H4 = H[re1:re2, 0:re1]
+    S4 = S[re1:re2, 0:re1]
+    V4 = V[re1:re2, 0:re1]
 
-array7 = [0 for i in range(14)]
-formula_H(array7, H7)
-formula_S(array7, S7)
-formula_V(array7, V7)
+    H5 = H[re1:re2, re1:re2]
+    S5 = S[re1:re2, re1:re2]
+    V5 = V[re1:re2, re1:re2]
 
-array8 = [0 for i in range(14)]
-formula_H(array8, H8)
-formula_S(array8, S8)
-formula_V(array8, V8)
+    H6 = H[re1:re2, re2:re3]
+    S6 = S[re1:re2, re2:re3]
+    V6 = V[re1:re2, re2:re3]
 
-array9 = [0 for i in range(14)]
-formula_H(array9, H9)
-formula_S(array9, S9)
-formula_V(array9, V9)
+    H7 = H[re2:re3, 0:re1]
+    S7 = S[re2:re3, 0:re1]
+    V7 = V[re2:re3, 0:re1]
 
-array = np.concatenate([array1, array2, array3, array4, array5, array6, array7, array8, array9])
+    H8 = H[re2:re3, re1:re2]
+    S8 = S[re2:re3, re1:re2]
+    V8 = V[re2:re3, re1:re2]
 
-print(array)
+    H9 = H[re2:re3, re2:re3]
+    S9 = S[re2:re3, re2:re3]
+    V9 = V[re2:re3, re2:re3]
 
-print("--- %s seconds ---" % (time.time() - start_time))
+    def formula_H(arr, H):
+        for i in range (len(H)):
+            for j in range (len(H[0])):
+                if (H[i][j] >= 316 and H[i][j] <= 360):
+                    arr[0] += 1
+                elif (H[i][j] >= 1 and H[i][j] < 26):
+                    arr[1] += 1
+                elif (H[i][j] >= 26 and H[i][j] < 41):
+                    arr[2] += 1
+                elif (H[i][j] >= 41 and H[i][j] < 121):
+                    arr[3] += 1
+                elif (H[i][j] >= 121 and H[i][j] < 191):
+                    arr[4] += 1
+                elif (H[i][j] >= 191 and H[i][j] < 271):
+                    arr[5] += 1
+                elif (H[i][j] >= 271 and H[i][j] < 295):
+                    arr[6] += 1
+                elif (H[i][j] >= 295 and H[i][j] < 316):
+                    arr[7] += 1
+                
+    def formula_S(arr, S):
+        for i in range (len(S)):
+            for j in range (len(S[0])):
+                if (S[i][j] >= 0 and S[i][j] < 0.2):
+                    arr[8] += 1
+                elif (S[i][j] >= 0.2 and S[i][j] < 0.7):
+                    arr[9] += 1
+                elif (S[i][j] >= 0.7 and S[i][j] <= 1):
+                    arr[10] += 1   
+        
+    def formula_V(arr, V):
+        for i in range (len(V)):
+            for j in range (len(V[0])):
+                if (V[i][j] >= 0 and V[i][j] < 0.2):
+                    arr[11] += 1
+                elif (V[i][j] >= 0.2 and V[i][j] < 0.7):
+                    arr[12] += 1
+                elif (V[i][j] >= 0.7 and V[i][j] <= 1):
+                    arr[13] += 1    
+
+    array1 = [0 for i in range(14)]
+    formula_H(array1, H1)
+    formula_S(array1, S1)
+    formula_V(array1, V1)
+
+    array2 = [0 for i in range(14)]
+    formula_H(array2, H2)
+    formula_S(array2, S2)
+    formula_V(array2, V2)
+
+    array3 = [0 for i in range(14)]
+    formula_H(array3, H3)
+    formula_S(array3, S3)
+    formula_V(array3, V3)
+
+    array4 = [0 for i in range(14)]
+    formula_H(array4, H4)
+    formula_S(array4, S4)
+    formula_V(array4, V4)
+
+    array5 = [0 for i in range(14)]
+    formula_H(array5, H5)
+    formula_S(array5, S5)
+    formula_V(array5, V5)
+
+    array6 = [0 for i in range(14)]
+    formula_H(array6, H6)
+    formula_S(array6, S6)
+    formula_V(array6, V6)
+
+    array7 = [0 for i in range(14)]
+    formula_H(array7, H7)
+    formula_S(array7, S7)
+    formula_V(array7, V7)
+
+    array8 = [0 for i in range(14)]
+    formula_H(array8, H8)
+    formula_S(array8, S8)
+    formula_V(array8, V8)
+
+    array9 = [0 for i in range(14)]
+    formula_H(array9, H9)
+    formula_S(array9, S9)
+    formula_V(array9, V9)
+
+    array = np.concatenate([array1, array2, array3, array4, array5, array6, array7, array8, array9])
+
+    return array
+
+# print(array)
+
+# print("--- %s seconds ---" % (time.time() - start_time))
 
 
 
--- api/venv/mcolor.py
+++ api/venv/mcolor.py
@@ -1,85 +1,56 @@
-import numpy as np
+# from PIL import Image
+import color
+import numpy as np 
 import cv2
 import os
-import functions
-import math
 import time
-
+import functions as fn
+# import texture as tex
 start_time = time.time()
-path = "C:/Users/Asus/Documents/Thea/SMT3/TubesAlgeo2/Algeo02-22012/dataset/73.jpg"
-image = cv2.imread(path)
-print(image)
-B,G,R = cv2.split(image)
-# cv2.imshow("Blue",B)
-# cv2.waitKey(0)
-# cv2.imshow("Green",G)
-# cv2.waitKey(0)
-# cv2.imshow("Red",R)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-print("WARNA BIRU")
-print(B)
-print("WARNA HIJAU")
-print(G)
-print("WARNA MERAH")
-print(R)
 
 
+# # membaca image inputan untuk dibandingkan
+# mainimage = "C:/Users/Asus/Pictures/Smiling cat.jpg"
+# # mainimage = "C:/Users/Asus/Documents/Thea/SMT3/TubesAlgeo2/Algeo02-22012/perforated/perforated_0001.jpg"
+# mimage = cv2.imread(mainimage)
+# len_row = len(mimage)
+# len_col = len(mimage[0])
+# mgreyscale = tex.konversigray(mimage)
+# mcooccurence = tex.createcooccurence(mgreyscale)
+# mtoople = fn.vectorcosine(mcooccurence) 
+# print(mtoople)
 
 
-
+# # baca semua image di dataset
+# dataset_dir = "C:/Users/Asus/Documents/Thea/SMT3/TubesAlgeo2/Algeo02-22012/testtext"
+# processedfiles = 0
+# final = []
+# # Check if the dataset directory exists
+# if os.path.exists(dataset_dir) and os.path.isdir(dataset_dir):
+#     image_files = os.listdir(dataset_dir)
     
+#     for filename in image_files:
+#         if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
+#             img_path = os.path.join(dataset_dir, filename)
+#             image = cv2.imread(img_path)
+#             len_row = len(image)
+#             len_col = len(image[0])
+#             greyscale = tex.konversigray(image)
+#             cooccurence = tex.createcooccurence(greyscale)
+#             toople = fn.vectorcosine(cooccurence) 
+#             # final.append(fn.cosinesimilarity(mtoople,toople))
+#             print(fn.cosinesimilarity(mtoople,toople))
+#             processedfiles += 1
+#             # print(f"Processedfiles{processedfiles}" )
+# final.sort()
+# print(final)
 
-print("\n Normalisasi matriks RGB\n")
-matB = B/255
-matG = G/255
-matR = R/255
+path1 = "C:/Users/HP/tubes-algeo2/dataset/9.jpg"
+path2 = "C:/Users/HP/tubes-algeo2/dataset/10.jpg"
+array1 = color.color(path1)
+array2 = color.color(path2)
+result = fn.cosinesimilarity(array1,array2)
 
-# Cmax = np.max(matB,matG,matR)
-# print(Cmax)
-print("\nMatriks CMAX\n")
-Cmax = np.maximum.reduce([matG,matB,matR])
-print(Cmax)
-print("\nMatriks CMIN\n")
-Cmin = np.minimum.reduce([matG,matB,matR])
-print(Cmin)
-
-print("\nMatriks Delta\n")
-Subtract = np.subtract(Cmax,Cmin)
-print(Subtract)
-
-len_row = len(Subtract)
-len_col = len(Subtract[0])
-
-matH = np.zeros((len_row, len_col))
-matS = np.zeros((len_row, len_col))
-for i in range (len_row):
-    for j in range (len_col):
-        if (Subtract[i][j]==0):
-            matH[i][j] = 0
-        elif (Cmax[i][j]==matR[i][j]):
-            matH[i][j] = 60*(((matG[i][j]-matB[i][j])/Subtract[i][j])%6)
-        elif ((Cmax[i][j])==matG[i][j]):
-            matH[i][j] = 60*(((matB[i][j]-matR[i][j])/Subtract[i][j])+2)
-        elif ((Cmax[i][j])==matG[i][j]):
-            matH[i][j] = 60*(((matR[i][j]-matG[i][j])/Subtract[i][j])+4)
-        if (Cmax[i][j]==0):
-            matS[i][j] = 0
-        elif (Cmax[i][j]!=0):
-            matS[i][j] = Subtract[i][j]/Cmax[i][j]
-matV = Cmax
-
-print("\nMatriks H\n")
-print(matH)
-print("\nMatriks S\n")
-print(matS)
-print("\nMatriks V\n")
-print(matV)
-
-veclanjar = []
-
-
-print(np.shape(matB))
-print(np.shape(B))
+print(result)
 
 print("--- %s seconds ---" % (time.time() - start_time))
